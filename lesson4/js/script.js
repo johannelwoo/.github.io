c545d81d4bb64fbbcf14d06dfc55a089f83bc997
johannelwoo <sch20048@byui.edu>
2021-02-06 20:10:33 -0700

    preston page continued

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,28 @@
+let currentDate = new Date().getDate();
+let currentMonth = Number(new Date().getMonth()) + 1;
+let currentYear = new Date().getFullYear();
+if (currentDate < 10) {
+  currentDate = "0" + currentDate;
+}
+if (currentMonth < 10) {
+  currentMonth = "0" + currentMonth;
+}
+
+document.getElementById("current-date").innerHTML =
+  currentDate + "/" + currentMonth + "/" + currentYear;
+
+
+
+const hamButton = document.querySelector(".menu");
+const menuUl = document.querySelector(".navigation");
+
+hamButton.addEventListener(
+"click",
+() => {
+    menuUl.classList.toggle("responsive");
+},
+false
+); 
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
  }
