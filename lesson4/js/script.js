b64d62738c8cff8bf53b99742c3cf1f89bfbffc2
johannelwoo <sch20048@byui.edu>
2021-02-11 17:15:31 -0700

    fixed issues

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,28 +1,7 @@
-let currentDate = new Date().getDate();
-let currentMonth = Number(new Date().getMonth()) + 1;
-let currentYear = new Date().getFullYear();
-if (currentDate < 10) {
-  currentDate = "0" + currentDate;
-}
-if (currentMonth < 10) {
-  currentMonth = "0" + currentMonth;
-}
-
-document.getElementById("current-date").innerHTML =
-  currentDate + "/" + currentMonth + "/" + currentYear;
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 
-
-const hamButton = document.querySelector(".menu");
-const menuUl = document.querySelector(".navigation");
-
-hamButton.addEventListener(
-"click",
-() => {
-    menuUl.classList.toggle("responsive");
-},
-false
-); 
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
- }
+  document.getElementById("mainNav").classList.toggle("hide");
+}
--- lesson5/js/hamburger.js
+++
@@ -1,6 +0,0 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.mainNav')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
